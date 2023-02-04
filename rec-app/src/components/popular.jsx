8429513e84efd3499d4e206219a83ec72aad698d
Amanda <amandarb43@gmail.com>
2023-02-04 12:17:53 -0800

    added in code to components and app.js/ experiencing issues with code and API

--- rec-app/src/components/popular.jsx
+++ rec-app/src/components/popular.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+
+
 function Popular() {
+
+    const[popular,setPopular] = useState([]);
+
+    useEffect(()=>{
+        getPopular();
+    },[]);
+
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apikey=${process.env.API_KEY}&number=5`);
+        const data = await api.json();
+        setPopular(data.recipes)
+        console.log(data);
+    }
   return (
-    <div>popular</div>
+    <div>
+        {popular.map((recipe) => {
+            return(
+                <div>
+                    <p>{recipe.title}</p>
+                </div>
+            );
+        })}
+    </div>
   )
 }
 
